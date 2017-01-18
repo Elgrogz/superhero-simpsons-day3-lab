93a0a8a199a6ba912308f50e837a166fc3396dd3
Elgrogz <gregorgilchrist@gmail.com>
2017-01-18 14:33:35 +0000

    food can take away health - tested

--- rat.js
+++ rat.js
@@ -2,4 +2,10 @@ var Rat = function(name) {
   this.name = name;
 }
 
+Rat.prototype = {
+  infect: function(food) {
+    food.calories *= -1;
+  }
+}
+
 module.exports = Rat;
--- superhero_specs/rat_spec.js
+++ superhero_specs/rat_spec.js
@@ -1,16 +1,24 @@
 var Rat = require('../rat');
+var Food = require('../food');
 var assert = require('assert')
 
 describe('Rat', function() {
 
   var itchy;
+  var donut;
 
   beforeEach(function() {
     itchy = new Rat("Itchy");
+    donut = new Food("Donut", 10);
   });
 
   it('rat can have a name', function() {
     assert.equal("Itchy", itchy.name);
   });
+
+  it('rat can infect food', function() {
+    itchy.infect(donut);
+    assert.equal(-10, donut.calories);
+  })
 
 })
