f74c3dde3e443a0a4b13d59999c404217125fc54
Elgrogz <gregorgilchrist@gmail.com>
2017-01-18 15:31:04 +0000

    can add character to springfield - tested

--- springfield.js
+++ springfield.js
@@ -2,4 +2,10 @@ var Springfield = function() {
   this.characters = [];
 }
 
+Springfield.prototype = {
+  add: function(character) {
+    this.characters.push(character);
+  }
+}
+
 module.exports = Springfield;
--- superhero_specs/springfield_spec.js
+++ superhero_specs/springfield_spec.js
@@ -1,16 +1,26 @@
+var Character = require('../character');
 var Springfield = require('../springfield');
 var assert = require('assert');
 
 describe('Springfield', function() {
 
+  var homer;
+  var mrBurns;
   var springfield;
 
   beforeEach(function() {
     springfield = new Springfield();
   });
+  homer = new Character("Homer", 200, "Donut", "Doh!");
+  mrBurns = new Character("Mr Burns", 100, "Catsup", "Excellent!");
 
   it('people array starts empty', function() {
     assert.equal(0, springfield.characters.length);
+  });
+
+  it('can add characters', function() {
+    springfield.add(homer);
+    assert.equal(1, springfield.characters.length);
   });
 
 })
