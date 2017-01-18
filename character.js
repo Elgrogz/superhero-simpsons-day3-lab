04010071c52efa8479b14e7128215595d88ec4e5
Elgrogz <gregorgilchrist@gmail.com>
2017-01-18 13:47:38 +0000

    name tested

--- character.js
+++ character.js
@@ -1,1 +1,8 @@
+var Character = function(name, health, favFood, catchphrase){
+  this.name = name;
+  this.health = health;
+  this.favFood = favFood;
+  this.catchphrase = catchphrase;
+}
+
 module.exports = Character;
